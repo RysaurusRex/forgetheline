c9d3a1ce79ed65859d946694415fc1c785959950
James Hoffman <forgetheline@gmail.com>
2026-04-28 17:46:03 -0500

    Remove invalid outputFileTracingIncludes from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
+ /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  outputFileTracingIncludes: {
-    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
-    '/*': ['./node_modules/.prisma/client/**/*'],
   },
 };
 
